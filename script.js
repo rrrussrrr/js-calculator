4538bc4204bbf78eed828675463930763e4ba855
rruebner <rruebner@gmail.com>
2022-05-14 21:50:33 -0400

    added basic math operator functions

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+
+    return a+b;
+}
+
+function sub(a, b) {
+
+    return a-b;
+}
+
+function mul(a, b) {
+
+    return a * b;
+}
+
+function div(a, b) {
+
+    return a / b;
+}
+
+function operate(operator, a, b){
+
+
+    
+}
