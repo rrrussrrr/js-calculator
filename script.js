f1094bf837e9522495716d7dfd95006a28181578
rruebner <rruebner@gmail.com>
2022-05-18 19:05:26 -0400

    tryin to get operations to work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var displayValue = 0;
-
+var firstValue = 0;
+var secondValue = 0;
+var currentOp = 0;
 
 
 function add(a, b) {
@@ -38,20 +39,40 @@ function operate(operator, a, b){
     }
 }
 
+function changeDisplay (num) {
+    const display = document.getElementById("display");
+        
+
+}
+
 const buttons = document.getElementById("buttons");
 
 buttons.addEventListener("click", function(e){
     const button = e.target;
-    const display = document.getElementById("display");
+    const primaryDisplay = document.getElementById("primary-display");
+    const resultDisplay = document.getElementById("result-display");
     const buttonText = button.innerText;
     if (button.classList.contains("num")) {
-        if (!Number.isNaN(buttonText)){
-            //Change Display Value
-            display.textContent += buttonText;
-            //Add new display value to variable
-            displayValue = display.textContent;
+        if (resultDisplay === ""){
+            //Still adding numbers
+            primaryDisplay.textContent += buttonText;
+        } else {
+            //new operation not using previous so clear
+            resultDisplay.textContent = "";
         }
-
+    } else if (button.classList.contains("op")) {
+        if (buttonText !== "=" && buttonText !== "Clear") {
+            currentOp = buttonText;
+            //Add new display value to variable
+            firstValue = primaryDisplay.textContent;
+            primaryDisplay.textContent = "";
+        }
+        else if (buttonText === "=") {
+            secondValue = primaryDisplay.textContent;
+            let result = operate(currentOp, firstValue, secondValue);
+            primaryDisplay.textContent = result;
+            currentOp = 0; 
+        }
     }
 
 
