a40f4e6175aac3ef11b216d6af147e484add1f6a
rruebner <rruebner@gmail.com>
2022-05-18 17:02:44 -0400

    added switch statement to operate function

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ function div(a, b) {
 
 function operate(operator, a, b){
 
-
-    
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return sub(a,b);
+        case "x":
+            return mul(a,b);
+        case "%":
+            if (b===0) {
+                return "ERROR: Divide By Zero";
+            } else return div(a,b);
+    }
 }
