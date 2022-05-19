4384064a26f26f0bc04e9c4f317bbf622d3d28d5
rruebner <rruebner@gmail.com>
2022-05-19 16:47:15 -0400

    made the calculator work mostly

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var firstValue = 0;
 var secondValue = 0;
 var currentOp = 0;
+var numMode = 0;
+var result = 0;
 
 
 function add(a, b) {
 
-    return a+b;
+    return Number(a)+Number(b);
 }
 
 function sub(a, b) {
@@ -53,28 +55,82 @@ buttons.addEventListener("click", function(e){
     const resultDisplay = document.getElementById("result-display");
     const buttonText = button.innerText;
     if (button.classList.contains("num")) {
-        if (resultDisplay === ""){
+        //if (resultDisplay === ""){
             //Still adding numbers
-            primaryDisplay.textContent += buttonText;
-        } else {
+            //primaryDisplay.textContent += buttonText;
+       // } else {
             //new operation not using previous so clear
+        if (currentOp === "="){
             resultDisplay.textContent = "";
+            currentOp = 0;
+        }
+        if (numMode === 0){ //if we just hit an operator key, enter new number mode
+            primaryDisplay.textContent = buttonText;
+            resultDisplay.textContent +=buttonText;
+            numMode = 1;
+        } else { //if not, we're in number mode and can continue entering our number
+            primaryDisplay.textContent += buttonText;
+            resultDisplay.textContent +=buttonText;
         }
     } else if (button.classList.contains("op")) {
-        if (buttonText !== "=" && buttonText !== "Clear") {
-            currentOp = buttonText;
-            //Add new display value to variable
-            firstValue = primaryDisplay.textContent;
-            primaryDisplay.textContent = "";
-        }
-        else if (buttonText === "=") {
+        if (buttonText !== "=" && buttonText !== "Clear") { //if it's not equal or clear
+            if (numMode === 1) { // if we just typed a number and now are operating
+                numMode = 0;
+                if (currentOp === 0 || currentOp === "=") { //if there wasn't a previous operation in the series
+                    currentOp = buttonText;
+                    //Add new display value to variable
+                    firstValue = primaryDisplay.textContent;
+                    //primaryDisplay.textContent = "";
+                    resultDisplay.textContent +=buttonText;
+                    
+                } 
+                else { //if there was a previous operation just operate and continue operating on result
+                    
+
+                    secondValue = primaryDisplay.textContent;
+                    result = operate(currentOp, firstValue, secondValue);
+
+                    currentOp = buttonText;
+                    primaryDisplay.textContent = result;
+                    //Add new display value to variable
+                    firstValue = result;
+                    resultDisplay.textContent +=buttonText;
+                    //currentOp = "="; //just operated
+
+                }
+            } else { //if we just entered another operation, just replace previous 
+                resultDisplay.textContent = resultDisplay.textContent.slice(0, -1);
+                currentOp = buttonText;
+                resultDisplay.textContent +=buttonText;
+            }
+
+         } 
+                
+
+            
+    
+        else if (buttonText === "=" && currentOp !== 0 && numMode === 1) { //equal only works with an entered #
+
             secondValue = primaryDisplay.textContent;
-            let result = operate(currentOp, firstValue, secondValue);
+            result = operate(currentOp, firstValue, secondValue);
             primaryDisplay.textContent = result;
-            currentOp = 0; 
+            currentOp = "="; 
+            primaryValue = result;
+            secondValue = 0;
+            console.log(firstValue + secondValue);
         }
+
+
     }
 
-
+    if (buttonText === "Clear") {
+        firstValue = 0;
+        secondValue = 0;
+        currentOp = 0;
+        numMode = 0;
+        result = 0;
+        primaryDisplay.textContent = "";
+        resultDisplay.textContent = "";
+    }
 
 })
